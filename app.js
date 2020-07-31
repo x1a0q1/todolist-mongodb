99bb173698af3e6652ef337b723dd8f191c147a4
x1a09i <x1a09i.dev@gmail.com>
2020-07-31 19:19:50 +0900

    Start to use mongoDB and initial task data

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const dateHelper = require(`${__dirname}/dateHelper.js`);
 
 const localPORT = 3000;
+const todolistDB = "todolistDB";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.set("view engine", "ejs");
+
+mongoose.connect(`mongodb://localhost:27017/${todolistDB}`, {
+  useNewUrlParser: true,
+});
+
+const taskSchema = new mongoose.Schema({
+  taskName: {
+    type: String,
+    required: [true, "I Need a Name!"],
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+// 3 Default Items for initial running
+const defaultTasks = [
+  new Task({
+    taskName: "Welcome to your todolist!",
+  }),
+  new Task({
+    taskName: "Hit the + button to add a new task.",
+  }),
+  new Task({
+    taskName: "<-- Hit this to complete a task.",
+  }),
+];
+
+Task.insertMany(defaultTasks, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default tasks to DB!");
+  }
+});
 
 const listName_work = "Work List";
 const itemList = ["Buy Food", "Cook Food", "Eat Food"];
